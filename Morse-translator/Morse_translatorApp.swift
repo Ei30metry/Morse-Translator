7cf89077207b65bc177705d9ca9f8b61558cb395
Artin <95337891+Ei30metry@users.noreply.github.com>
2023-02-04 23:15:34 +0330

    Initial Commit

---
+++ Morse-translator/Morse_translatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Morse_translatorApp.swift
+//  Morse-translator
+//
+//  Created by Artin on 11/15/1401 AP.
+//
+
+import SwiftUI
+
+@main
+struct Morse_translatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
